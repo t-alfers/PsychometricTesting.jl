5c91ff05fb5f4b9a3f9c8c99abb27463fd2a4102
Documenter.jl <documenter@juliadocs.github.io>
2022-04-20 08:17:24 +0000

    build based on 969d24a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PsychometricTesting","category":"page"},{"location":"#PsychometricTesting","page":"Home","title":"PsychometricTesting","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PsychometricTesting.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PsychometricTesting]","category":"page"}]
+}
